84542e22e8a9490b5977ffda765facf77f1177d1
Maicon Vinicios <maiconvos@gmail.com>
2017-06-23 12:17:07 -0300

    Added sizeof.c file.

--- chapter-02/sizeof.c
+++ chapter-02/sizeof.c
@@ -1,9 +1,12 @@
 /*
-  A simple example how to use the sizeof operator to know the size of a type.
+  A simple example how to use the sizeof operator to know the size in bytes a type.
   Author: @maiconvos
 */
 #include <stdio.h>
 
 void main() {
-
+  printf("The size in bytes of int type is: %ld\n", sizeof(int));
+  printf("The size in bytes of float type is: %ld\n", sizeof(float));
+  printf("The size in bytes of double type is: %ld\n", sizeof(double));
+  printf("The size in bytes of char type is: %ld\n", sizeof(char));
 }
