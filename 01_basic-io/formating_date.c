de7103f9ea0be42d65ce1bf99d3f31d16961a1ea
Maicon Vinicios <maiconvos@gmail.com>
2017-06-26 21:05:39 -0300

    Changed the name of chapter-02 to 01_basico-io

---
+++ 01_basic-io/formating_date.c
@@ -0,0 +1,21 @@
+/*
+  Program asks a user to enter a date then shows it in two formats.
+  Author: @maiconvos
+*/
+#include <stdio.h>
+
+void main() {
+  int year, month, day;
+
+  printf("Enter with a year:");
+  scanf("%d", &year);
+
+  printf("Enter with a month number:");
+  scanf("%d", &month);
+
+  printf("Enter with a day number:");
+  scanf("%d", &day);
+
+  printf("You enter the date %d-%2d-%2d\n", year, month, day);
+  printf("The date in brazilian format %2d/%2d/%d\n", day, month, year);
+}
