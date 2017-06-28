8ea7b996e16d38b1b66480a62e25cba4bb520399
Maicon Vinicios <maiconvos@gmail.com>
2017-06-28 00:50:40 -0300

    Added the 02_conditionals/leap_year.c

---
+++ 02_conditionals/leap_year.c
@@ -0,0 +1,19 @@
+/*
+  Program that shows if a enter year is or not a leap year.
+  Author: @maiconvos
+*/
+#include <stdio.h>
+
+void main() {
+
+  int year;
+
+  printf("Enter with a year value: ");
+  scanf("%d", &year);
+
+  if (year % 4 == 0 || (year % 100 == 0 && year % 400 != 0)) {
+    printf("The %d is (was) a leap year\n", year);
+  } else {
+    printf("The %d is not a leap year\n", year);
+  }
+}
