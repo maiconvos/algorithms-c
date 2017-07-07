87a16029d3c9587a0ff1e04aaa7c6fa3a287d909
Maicon Vinicios <maiconvos@gmail.com>
2017-07-06 21:48:44 -0300

    Added 04_functions/show_hours.c

---
+++ 04_functions/show_hours.c
@@ -0,0 +1,21 @@
+/*
+  Program takes a number of hours and shows the number of seconds in it.
+  Author: @maiconvos.
+*/
+#include <stdio.h>
+
+long int hours_to_sec(long int hours) {
+  return hours * 3600;
+}
+
+void main() {
+  int hours = 0;
+  int out = 0;
+
+  while(!out) {
+    printf("Enter the number of hours (Press Ctrl + C to exit):");
+    scanf("%d", &hours);
+
+    printf("%d hour(s) has (have) %ld second(s).\n", hours, hours_to_sec(hours));
+  }
+}
