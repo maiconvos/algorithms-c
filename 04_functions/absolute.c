da0b511e2d76689cb0f0d0b5700b65a71928836d
Maicon Vinicios <maiconvos@gmail.com>
2017-07-06 21:14:46 -0300

    Created 04_functions folder and added 04_functions/absolute.c

---
+++ 04_functions/absolute.c
@@ -0,0 +1,24 @@
+/*
+  Program takes a number and returns its absolute value. Example: Enter -5 and returns 5.
+  Author: @maiconvos
+*/
+#include <stdio.h>
+
+int abs(int number){
+  if (number >= 0 ) {
+    return number;
+  } else {
+    return (-2 * number) - number;
+  }
+}
+
+void main() {
+  int value = 0;
+  int out = 0;
+
+  while(!out) {
+    printf("Enter a number (press CTRL + C to exit): ");
+    scanf("%d", &value);
+    printf("Absolute value is: %d\n", abs(value));
+  }
+}
